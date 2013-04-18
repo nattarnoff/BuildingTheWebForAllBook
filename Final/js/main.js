8229815d0a202f43f9865722680cd6576a3f8bc6
gtarnoff <greg.tarnoff@gmail.com>
2013-04-18 16:41:18 -0500

    CSS changes for pull quotes. Updated pullQuote JS

--- Final/js/main.js
+++ Final/js/main.js
@@ -1,15 +1,17 @@
 var pullquotes = $('span.pullquote');
 var PullTheQuotes = function(){
 	console.log(pullquotes);
-	// pullquotes.forEach(function(){
-		var text = pullquotes.text();
-		console.log(text);
-		var html = "<blockquote class='pullquote'>"+text+"</blockquote>";
-		console.log(html);
-		console.log(pullquotes.parents('.row'));
-		pullquotes.parents('.row').children('p:first-child').after(html);
+	// console.log(pullquotes.length);
+	 for(i=0;i<pullquotes.length;i++){
+		var text = $(pullquotes[i]).text();
+		var html = "<blockquote class='pullquote";
+		if($(pullquotes[i]).hasClass('right')){
+			html += " right";
+		}
+		html += "'>"+text+"</blockquote>";
+		$(pullquotes[i]).parents('.row').children('p:first-child').after(html);
 		
-	// })
+	 };
 }
 
 $(function(){
