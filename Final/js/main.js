47b0510fef8ce92952f3936f4e8a979e3cb4e5c1
gtarnoff <greg.tarnoff@gmail.com>
2013-04-18 15:30:16 -0500

    CSS changes. Background images. JS pull quote generator.

--- Final/js/main.js
+++ Final/js/main.js
@@ -1,1 +1,17 @@
+var pullquotes = $('span.pullquote');
+var PullTheQuotes = function(){
+	console.log(pullquotes);
+	// pullquotes.forEach(function(){
+		var text = pullquotes.text();
+		console.log(text);
+		var html = "<blockquote class='pullquote'>"+text+"</blockquote>";
+		console.log(html);
+		console.log(pullquotes.parents('.row'));
+		pullquotes.parents('.row').children('p:first-child').after(html);
+		
+	// })
+}
 
+$(function(){
+	PullTheQuotes();
+})
